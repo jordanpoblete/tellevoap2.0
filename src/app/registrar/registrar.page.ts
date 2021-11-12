c69604ad4f6ac47ffd36ac641771e074a1d27594
Jordan <jor.pobletem@duocuc.cl>
2021-11-12 18:30:44 -0300

    pichula grande

--- src/app/registrar/registrar.page.ts
+++ src/app/registrar/registrar.page.ts
@@ -3,6 +3,7 @@ import { CrudService } from '../crud.service';
 
 
 
+
 @Component({
   selector: 'app-registrar',
   templateUrl: './registrar.page.html',
@@ -21,11 +22,20 @@ export class RegistrarPage implements OnInit {
 
   async agregar(txtUsuario:HTMLInputElement, txtClave:HTMLInputElement)
   {
-    const datos = [{"usuario": txtUsuario.value,
-                    "clave": txtClave.value }]
-    await this.crud.agregarConKey(txtUsuario.value, datos);
-    txtUsuario.value = "";
-    txtClave.value = "";
+    const valor = await this.crud.rescatar(txtUsuario.value)
+    console.log(valor)
+    if (valor == null)
+    {        
+      const datos = [{"usuario": txtUsuario.value,
+                      "clave": txtClave.value }]
+      await this.crud.agregarConKey(txtUsuario.value, datos);
+      txtUsuario.value = "";
+      txtClave.value = "";
+    }
+    else
+    {
+      console.log("Ya existe")
+    }
   }
   async buscar(txtUsuario:HTMLInputElement)
   {
@@ -39,9 +49,53 @@ export class RegistrarPage implements OnInit {
     await this.crud.eliminar(txtUsuario.value);
   }
 
+  async actualizar(txtUsuario:HTMLInputElement, txtClave:HTMLInputElement)
+  {
+    const valor = await this.crud.rescatar(txtUsuario.value);
+    this.usuario = valor[0].usuario;
+    this.clave = valor[0].clave;
+
+    if (txtUsuario.value.trim().length != 0)
+    {
+      this.usuario = txtUsuario.value;
+    }
+  
+    if (txtClave.value.trim().length != 0)
+    {
+      this.clave = txtClave.value;
+    }
+
+
+    const datos = [{"usuario": txtUsuario.value,
+                    "clave": txtClave.value }];
+    await this.crud.agregarConKey(txtUsuario.value,datos);
+    txtUsuario.value = "";
+    txtClave.value = "";
+  }
+
   async listar()
   {
     this.lista = this.crud.listar();
+  }
+
+  async con(txtUsuario:HTMLInputElement, txtClave:HTMLInputElement)
+  {
+    const valor = await this.crud.rescatar(txtUsuario.value);
+    if(valor != null)
+    {
+      if(valor[0].clave === txtClave.value)
+      {
+        console.log("logueado exitosamente")
+      }
+      else
+      {
+        console.log("clave incorrecta")
+      }
+    }
+    else
+    {
+      console.log("No existe usuario")
+    }
   }
 
 }
